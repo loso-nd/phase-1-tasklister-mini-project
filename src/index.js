2eb446aed1348a05d205911b69ff82707bd42b2f
Nahum <losond8@gmail.com>
2021-06-02 10:08:52 -0700

    Init and targe the form to addEventlistenr,  create li element and post list to the DOM.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // init and grab the form
+  const taskForm = document.querySelector('#create-task-form')
+
+  //addEventListener to the form and pass a function that handles the submit
+  taskForm.addEventListener('submit', handleSubmit)
+
+  //handleSubmit method that takes an event as an arg
+  //When submit button is triggered it should target the user input  and display it on the DOM
+  function handleSubmit(e){
+    e.preventDefault(); //prevents auto refresh
+    console.log(e.target['new-task-description'].value);
+
+
+  //Grab the Ul for the list
+  //create an li element to display task when triggered by user input
+  //create a delete btn
+  let toDoList = document.querySelector('#list')
+  let list = document.createElement('li');
+  list.textContent = e.target["new-task-description"].value
+  
+  //Display li to the DOM
+  toDoList.appendChild(list);
+
+  }
+
+
+
 });
